e6872c950d769595c30ba046f592d642f06383bc
badistricia <3347977962@qq.com>
2022-07-28 22:03:46 +0800

    Article build

---
+++ badistricia-Blog/src/main/java/com/badistricia/BadistriciaBlogApplication.java
@@ -0,0 +1,18 @@
+package com.badistricia;
+
+import com.sun.glass.ui.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author badistricia
+ * @version 1.0
+ * @description :
+ * @date 2022/7/28 8:31:54
+ */
+@SpringBootApplication
+public class BadistriciaBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BadistriciaBlogApplication.class,args);
+    }
+}
---
+++ badistricia-framework/src/main/java/com/badistricia/domain/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.badistricia.domain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author badistricia
+ * @version 1.0
+ * @description :
+ * @date 2022/7/28 21:59:41
+ */
+@RestController
+public class ArticleController {
+
+}
